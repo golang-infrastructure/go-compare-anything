07270b8c6e81550386c416426e1cb5bbada5c9e7
CC11001100 <CC11001100@qq.com>
2022-10-31 00:22:14 +0800

    init

--- compare.go
+++ compare.go
@@ -1,7 +1,24 @@
 package compare_anything
 
+import "reflect"
+
 // Compare 比较大小
 func Compare(a, b any) int {
 	// TODO
+
+	reflect.TypeOf("").PkgPath()
+
 	return -1
 }
+
+func GenComparatorFor[T any](value T) (Comparator[T], error) {
+	// TODO
+	return nil, nil
+}
+
+//func GenComparatorFromType() {
+//	of := reflect.TypeOf(value)
+//	switch of.Kind() {
+//
+//	}
+//}
