8d27c5c5a1f601a8ce512bbfaa7762bb0e12c32e
CC11001100 <CC11001100@qq.com>
2022-11-24 01:00:43 +0800

    add comparable.go

--- comparable.go
+++ comparable.go
@@ -1,5 +1,33 @@
 package compare_anything
 
+import "reflect"
+
+// Comparable 表示是可比较的
 type Comparable interface {
+
+	// CompareTo 用来做比较的方法
 	CompareTo(target any) int
+}
+
+// IsComparable 判断是否是可比较类型
+func IsComparable(v any) bool {
+	reflectValue := reflect.ValueOf(v)
+	if !reflectValue.CanInterface() {
+		return false
+	}
+	_, ok := reflectValue.Interface().(Comparable)
+	return ok
+}
+
+// CastToComparable 把给定的值转为Comparable接口类型，如果无法转换则会返回error
+func CastToComparable(value any) (Comparable, error) {
+	reflectValue := reflect.ValueOf(value)
+	if !reflectValue.CanInterface() {
+		return nil, ErrCastFailed
+	}
+	v, ok := reflectValue.Interface().(Comparable)
+	if !ok {
+		return nil, ErrCastFailed
+	}
+	return v, nil
 }
---
+++ errors.go
@@ -0,0 +1,5 @@
+package compare_anything
+
+import "errors"
+
+var ErrCastFailed = errors.New("type cast failed")
